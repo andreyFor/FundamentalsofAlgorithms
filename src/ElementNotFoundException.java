065375de05f4c7c8d4b46fe49f5eab4546c97a60
Pronyushkin_qa <kollaydereva@gmail.com>
2022-11-07 19:45:57 +0300

    1

--- src/ElementNotFoundException.java
+++ src/ElementNotFoundException.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class ElementNotFoundException {
+public class ElementNotFoundException extends RuntimeException {
+    public ElementNotFoundException() {
+    }
+
+    public ElementNotFoundException(String message) {
+        super(message);
+    }
+
+    public ElementNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ElementNotFoundException(Throwable cause) {
+        super(cause);
+    }    //1
+
+
+    public ElementNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
--- src/InvalidationException.java
+++ src/InvalidationException.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class InvalidationException {
+public class InvalidationException extends RuntimeException {
+
+    public InvalidationException() {
+    }
+
+    public InvalidationException(String message) {
+        super(message);
+    }
+
+    public InvalidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidationException(Throwable cause) {
+        super(cause);
+    }
+
+    public InvalidationException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
+//1
